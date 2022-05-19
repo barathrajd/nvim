b57917719b6fddeae1739a223debff5419bf922f
barathrajd <barathdeva757@gmail.com>
2022-05-19 19:41:39 +0530

    lazygit

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -41,9 +41,9 @@ vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
--- function _LAZYGIT_TOGGLE()
--- 	lazygit:toggle()
--- end
+function _LAZYGIT_TOGGLE()
+	lazygit:toggle()
+end
 
 local node = Terminal:new({ cmd = "node", hidden = true })
 
