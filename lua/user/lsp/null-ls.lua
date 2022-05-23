c892667817d6dd466622c0c688dca96adaf8075d
barathrajd <barathdeva757@gmail.com>
2022-05-23 21:18:29 +0530

    formatter changes

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -7,11 +7,30 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier,
+		diagnostics.eslint,
+		code_actions.eslint,
+		formatting.prettier.with({
+			filetypes = {
+				"javascript",
+				"typescript",
+				"javascriptreact",
+				"typescriptreact",
+				"css",
+				"scss",
+				"html",
+				"json",
+				"yaml",
+				"markdown",
+				"graphql",
+				"md",
+				"txt",
+			},
+		}),
 		formatting.stylua,
 	},
 	on_attach = function(client)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,7 +70,7 @@ return packer.startup(function(use)
 	-- LSP
 	use("neovim/nvim-lspconfig") -- enable LSP
 	use("williamboman/nvim-lsp-installer") -- simple to use language server nvim-lsp-installer
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+	use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } }) -- for formatters and linters
 
 	-- Telescope
 	use("nvim-telescope/telescope.nvim")
