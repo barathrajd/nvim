1870c4047f0c57b8e5f0668962b6a202165711fc
barathrajd <barathdeva757@gmail.com>
2022-06-22 13:47:51 +0530

    colorized added

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -22,6 +22,7 @@ nvim_tree.setup({
 	-- disable_netrw = true,
 	-- hijack_netrw = true,
 	-- open_on_setup = false,
+	auto_reload_on_write = true,
 	ignore_ft_on_setup = {
 		"startify",
 		"dashboard",
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,6 +66,7 @@ return packer.startup(function(use)
 	use("saadparwaiz1/cmp_luasnip") -- snippet completions
 	use("hrsh7th/cmp-nvim-lsp")
 	use("hrsh7th/cmp-nvim-lua")
+	use("norcalli/nvim-colorizer.lua")
 
 	-- snippets
 	use("L3MON4D3/LuaSnip") --snippet engine
