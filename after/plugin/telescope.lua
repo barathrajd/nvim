58e142f9d38942d9be32935675d7442a0ffd0fce
barathrajd <barathdeva757@gmail.com>
2022-12-19 15:04:05 +0530

    Telescope conf added and the remaps keys changed

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,1 +1,28 @@
-require("telescope").setup({ defaults = { file_ignore_patterns = { "node_modules" } } })
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = { "node_modules" },
+		previewer = false,
+	},
+	pickers = {
+		find_files = {
+			previewer = false,
+			theme = "dropdown",
+		},
+		live_grep = {
+			theme = "dropdown",
+			previewer = false,
+		},
+		buffers = {
+			theme = "dropdown",
+			previewer = false,
+		},
+		git_files = {
+			theme = "dropdown",
+			previewer = false,
+		},
+		grep_string = {
+			theme = "dropdown",
+			previewer = false,
+		},
+	},
+})
