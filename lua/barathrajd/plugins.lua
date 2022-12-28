5a6a645c868c574b147c22dee2480439eab8edb9
barathrajd <barathdeva757@gmail.com>
2022-12-28 22:23:21 +0530

    Added the Intent Line

--- lua/barathrajd/plugins.lua
+++ lua/barathrajd/plugins.lua
@@ -121,7 +121,7 @@ return packer.startup(function(use)
   use("tpope/vim-fugitive")
   use("MunifTanjim/prettier.nvim")
   use({ 'akinsho/bufferline.nvim', tag = "v3.*" })
-  -- use("lukas-reineke/indent-blankline.nvim")
+  use("lukas-reineke/indent-blankline.nvim")
   if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
